856fa8b94351b82e6856b2fdb259aa67bab1a1ee
Omar Gudino <omfegumo6@gmail.com>
2013-04-28 17:36:35 -0700

    Initial Client commit

---
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/FDDPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FDDPAppDelegate.h
+//  FondaDeDonPanchito
+//
+//  Created by Omar Gudino on 4/28/13.
+//  Copyright (c) 2013 Omar Gudino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FDDPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FondaDeDonPanchito
+//
+//  Created by Omar Gudino on 4/28/13.
+//  Copyright (c) 2013 Omar Gudino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FDDPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FDDPAppDelegate class]));
+    }
+}

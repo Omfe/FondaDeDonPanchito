4437a3586ce3168aa0118cf7f5898b47463dd33f
Omar Gudino <omfegumo6@gmail.com>
2013-05-17 21:38:52 -0700

    Added AuthenticationController

---
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/Controllers/FDDPAuthenticationController.h
@@ -0,0 +1,13 @@
+//
+//  FDDPAuthenticationController.h
+//  FondaDeDonPanchito
+//
+//  Created by Omar Gudino on 5/17/13.
+//  Copyright (c) 2013 Omar Gudino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FDDPAuthenticationController : NSObject
+
+@end
---
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/Controllers/FDDPAuthenticationController.m
@@ -0,0 +1,13 @@
+//
+//  FDDPAuthenticationController.m
+//  FondaDeDonPanchito
+//
+//  Created by Omar Gudino on 5/17/13.
+//  Copyright (c) 2013 Omar Gudino. All rights reserved.
+//
+
+#import "FDDPAuthenticationController.h"
+
+@implementation FDDPAuthenticationController
+
+@end

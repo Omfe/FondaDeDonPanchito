9000de7d99187404a6e061d7a5733c9e938bd7c6
Omar Gudino <omfegumo6@gmail.com>
2013-05-17 21:50:10 -0700

    Starting to do Auth Methods

--- Server/app.rb
+++ Server/app.rb
@@ -5,6 +5,7 @@ require "sinatra"
 
 @@mysqlclient = Mysql2::Client.new(host: "localhost", username: "root", database: "FondadeDonPanchito")
 
+load "routes/auth.rb"
 load "routes/index.rb"
 load "routes/items.rb"
 load "routes/meals.rb"
---
+++ Server/routes/auth.rb
@@ -0,0 +1,14 @@
+# http://localhost:4567/login
+post '/login' do
+  
+end
+
+# http://localhost:4567/logout
+post '/logout' do
+  
+end
+
+# http://localhost:4567/register
+post '/register' do
+  
+end

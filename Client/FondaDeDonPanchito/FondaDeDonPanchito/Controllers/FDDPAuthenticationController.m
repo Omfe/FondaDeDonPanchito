441b55b051643cd8b19ca249d46cf54086ac819f
Rogelio Gudino <cananito@gmail.com>
2013-05-19 10:44:08 -0700

    Boilerplate code as starting point.

--- Client/FondaDeDonPanchito/FondaDeDonPanchito/Controllers/FDDPAuthenticationController.m
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/Controllers/FDDPAuthenticationController.m
@@ -8,10 +8,17 @@
 
 #import "FDDPAuthenticationController.h"
 
-static FDDPAuthenticationController *_sharedAuthenticationController = nil;
+#define kServerURL @"http://localhost:4567"
+
+@interface FDDPAuthenticationController ()
+
+//
+
+@end
 
 @implementation FDDPAuthenticationController
 
+static FDDPAuthenticationController *_sharedAuthenticationController = nil;
 + (FDDPAuthenticationController *)sharedAuthenticationController
 {
     if (!_sharedAuthenticationController) {
@@ -23,7 +30,34 @@ static FDDPAuthenticationController *_sharedAuthenticationController = nil;
 #pragma mark - Public Methods
 - (void)loginWithUsername:(NSString *)username withPassword:(NSString *)password
 {
+    NSMutableURLRequest *urlRequest;
+    NSURL *url;
+    NSString *urlString;
+    NSData *bodyData;
+    NSDictionary *bodyDictionary;
     
+    
+    urlString = [kServerURL stringByAppendingPathComponent:@"login"];
+    url = [NSURL URLWithString:urlString];
+    urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest setHTTPMethod:@"POST"];
+    
+    bodyDictionary = @{ @"username": username, @"password": password };
+    bodyData = [NSJSONSerialization dataWithJSONObject:bodyDictionary options:0 error:nil];
+    [urlRequest setHTTPBody:bodyData];
+    
+    [NSURLConnection sendAsynchronousRequest:urlRequest queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *urlResponse, NSData *data, NSError *error) {
+        NSDictionary *responseDictionary;
+        
+        if (error) {
+            NSLog(@"%@", error.localizedDescription);
+            return;
+        }
+        
+        responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"Message: %@", responseDictionary[@"message"]);
+        NSLog(@"Token: %@", responseDictionary[@"token"]);
+    }];
 }
 
 @end

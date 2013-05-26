b1e473b6303058763fb22e8620bed468384a24fa
Rogelio Gudino <cananito@gmail.com>
2013-05-25 20:11:32 -0700

    Stopped using "_" as a prefix for private methods (following documentation recommendation), setting correct titles on the VCs in initializers, using correct initializers and getting rid of the back button.

--- Client/FondaDeDonPanchito/FondaDeDonPanchito/ViewControllers/FDDPUserInfoViewController.m
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/ViewControllers/FDDPUserInfoViewController.m
@@ -11,28 +11,34 @@
 #import "FDDPUser.h"
 
 @interface FDDPUserInfoViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *firstNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *lastNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UIButton *UIButtonlogOutButton;
 
-
 @end
 
 @implementation FDDPUserInfoViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"User Info";
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
     FDDPUser *user;
+    [super viewDidLoad];
     
     user = [[FDDPAuthenticationManager sharedManager] loggedInUser];
     self.firstNameLabel.text = user.firstName;
     self.lastNameLabel.text = user.lastname;
     self.usernameLabel.text = user.username;
-    
-    NSLog(@"%@", user.firstName);
 }
 
 @end

227e296cf5977fe8f6a569b367caf4e155f7e213
Rogelio Gudino <cananito@gmail.com>
2013-06-04 22:14:33 -0700

    Skeleton for removing objects from orders.

--- Client/FondaDeDonPanchito/FondaDeDonPanchito/ViewControllers/FDDPMealsAndItemsViewController.m
+++ Client/FondaDeDonPanchito/FondaDeDonPanchito/ViewControllers/FDDPMealsAndItemsViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "FDDPMealsAndItemsViewController.h"
+#import "FDDPMeal.h"
+#import "FDDPItem.h"
 
-@interface FDDPMealsAndItemsViewController ()
+@interface FDDPMealsAndItemsViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
@@ -18,7 +20,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        self.title = @"Remove";
     }
     return self;
 }
@@ -26,6 +28,62 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupUI];
+}
+
+
+#pragma mark - UITableViewDataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.orderObjectsArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell;
+    static NSString *identifier = @"MealsAndItemsTableViewCellIdentifier";
+    id object;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    object = [self.orderObjectsArray objectAtIndex:indexPath.row];
+    if ([object isKindOfClass:[FDDPMeal class]]) {
+        cell.textLabel.text = [(FDDPMeal *)object mealName];
+    } else if ([object isKindOfClass:[FDDPItem class]]) {
+        cell.textLabel.text = [(FDDPItem *)object itemName];
+    }
+    
+    return cell;
+}
+
+
+#pragma mark - UITableViewDelegate Methods
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self.orderObjectsArray removeObjectAtIndex:indexPath.row];
+    if (self.completionBlock) {
+        self.completionBlock();
+    }
+}
+
+
+#pragma mark - Action Methods
+- (IBAction)cancelWasPressed:(id)sender
+{
+    if (self.completionBlock) {
+        self.completionBlock();
+    }
+}
+
+
+#pragma mark - Private Methods
+- (void)setupUI
+{
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelWasPressed:)];
 }
 
 @end
